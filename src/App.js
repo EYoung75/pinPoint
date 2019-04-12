1f89f97fcf05a64136456e288de6bac89ec6d752
EYoung75 <eyoung75.ey@gmail.com>
2019-04-12 12:40:49 -0600

    initialized repo and installed reactstrap, bootstrap, react-router, react-redux and redux dependencies. Additionally, began styling of navbar component, structuring app flow and end goal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import Navigation from "./components/Navigation.jsx"
 
 class App extends Component {
+  state = {
+    collapsed: false
+  };
+
+  toggleNavbar = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navigation toggleNavbar={this.toggleNavbar} {...this.state}/>
       </div>
     );
   }
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu
+} from "reactstrap";
+
+const Navigation = props => {
+  return (
+    <Container className={"navigation"}>
+      <Navbar light expand="md">
+        <NavbarBrand href="/">PinPoint | Denver</NavbarBrand>
+        <NavbarToggler onClick={props.toggleNavbar} />
+        <Collapse isOpen={props.collapsed} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="">Login</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="">Sign-Up</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret />
+              <DropdownMenu right>
+                <DropdownItem>About</DropdownItem>
+                <DropdownItem>FAQ's</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Contact</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </Container>
+  );
+};
+
+export default Navigation;
