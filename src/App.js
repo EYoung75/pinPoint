667842c8f7353192b24b6f6f095d47ea1b222852
EYoung75 <eyoung75.ey@gmail.com>
2019-04-16 12:03:16 -0600

    navigating api parameters around data wanted to display when app is built up

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   };
 
   componentDidMount() {
-    fetch(`https://api.foursquare.com/v2/venues/explore?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}&v=20180323&limit=1&ll=40.7243,-74.0018&query=coffee`)
+    fetch(`https://api.foursquare.com/v2/venues/search?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}&v=20180323&near=Denver,CO&intent=browse&query=food`)
     .then(res => res.json())
     .then(data => console.log(data.response))
   }
